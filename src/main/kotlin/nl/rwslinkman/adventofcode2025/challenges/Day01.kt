c95f7b1d0875c77b8e5c6f074f299d6a005a2d23
Rick Slinkman <rw.slinkman@gmail.com>
2025-12-01 09:06:00 +0100

    feat: Solution for Day01

--- src/main/kotlin/nl/rwslinkman/adventofcode2025/challenges/Day01.kt
+++ src/main/kotlin/nl/rwslinkman/adventofcode2025/challenges/Day01.kt
@@ -6,11 +6,63 @@ import nl.rwslinkman.adventofcode2025.Puzzle
 @Puzzle("day01/input.txt")
 object Day01: AdventChallenge {
 
+    private const val LEFT = 'L'
+    private const val RIGHT = 'R'
+
     override fun part1(inputString: String): Any {
-        return 0L
+        val instructions = inputString.lines().map { line ->
+            line[0] to line.substring(1).toInt()
+        }
+
+        var dialPosition = 50
+        var zeroCounter = 0
+        instructions.forEach { (k, v) ->
+            when(k) {
+                LEFT -> {
+                    dialPosition -= (v % 100)
+                    if(dialPosition < 0) {
+                        dialPosition += 100
+                    }
+                }
+                RIGHT -> {
+                    dialPosition += (v % 100)
+                    dialPosition %= 100
+                }
+            }
+            if(dialPosition == 0) zeroCounter++
+        }
+        return zeroCounter
     }
 
     override fun part2(inputString: String): Any {
-        return 0L
+        val instructions = inputString.lines().map { line ->
+            line[0] to line.substring(1).toInt()
+        }
+
+        var dialPosition = 50
+        var zeroCounter = 0
+        instructions.forEach { (k, v) ->
+            when(k) {
+                LEFT -> {
+                    repeat(v) {
+                        dialPosition -= 1
+                        dialPosition %= 100
+                        if(dialPosition == 0) {
+                            zeroCounter++
+                        }
+                    }
+                }
+                RIGHT -> {
+                    repeat(v) {
+                        dialPosition += 1
+                        dialPosition %= 100
+                        if(dialPosition == 0) {
+                            zeroCounter++
+                        }
+                    }
+                }
+            }
+        }
+        return zeroCounter
     }
 }
--- src/test/kotlin/nl/rwslinkman/adventofcode2025/challenges/Day01Test.kt
+++ src/test/kotlin/nl/rwslinkman/adventofcode2025/challenges/Day01Test.kt
@@ -7,10 +7,53 @@ class Day01Test {
 
     @Test
     fun `example part 1`() {
-        val exampleData = ""
+        val exampleData = "L68\n" +
+                "L30\n" +
+                "R48\n" +
+                "L5\n" +
+                "R60\n" +
+                "L55\n" +
+                "L1\n" +
+                "L99\n" +
+                "R14\n" +
+                "L82"
 
         val result = Day01.part1(exampleData)
 
-        assertEquals(0L, result)
+        assertEquals(3, result)
+    }
+
+    @Test
+    fun `example part 2`() {
+        val exampleData = "L68\n" +
+                "L30\n" +
+                "R48\n" +
+                "L5\n" +
+                "R60\n" +
+                "L55\n" +
+                "L1\n" +
+                "L99\n" +
+                "R14\n" +
+                "L82"
+
+        val result = Day01.part2(exampleData)
+
+        assertEquals(6, result)
+    }
+
+    @Test
+    fun `second example part 2`() {
+        val exampleData = "R1000"
+        val result = Day01.part2(exampleData)
+
+        assertEquals(10, result)
+    }
+
+    @Test
+    fun `third example part 2`() {
+        val exampleData = "L1000"
+        val result = Day01.part2(exampleData)
+
+        assertEquals(10, result)
     }
 }
